fbd7bdbaf5bc1b06eb72dce45c68c10bb4f26e1d
JohnyzHub <jbshaik82@gmail.com>
2018-12-09 22:34:21 -0500

    Java-Concurrency: Dead-lock demo
    	Shows different ways of obtaining thread dead-lock.

---
+++ src/main/java/com/myjava/concurrency/basics/deadlock/TestDeadLock1.java
@@ -0,0 +1,25 @@
+package com.myjava.concurrency.basics.deadlock;
+
+/**
+ * @author johnybasha
+ *
+ */
+public class TestDeadLock1 {
+
+	public static void main(String[] args) {
+
+		School school = new School();
+
+		Thread t1 = new Thread(() -> {
+			school.method1();
+		}, "t1");
+
+		Thread t2 = new Thread(() -> {
+			school.method3();
+		}, "t2");
+
+		t1.start();
+		t2.start();
+	}
+
+}
